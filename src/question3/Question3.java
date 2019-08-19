933984bb679472b45b3ad997c53e26f0c9a37607
yash.gautam_cs18 <yash.gautam_cs18@gla.ac.in>
2019-08-19 14:53:06 +0530

    add class:Question4

--- src/question3/Question3.java
+++ src/question3/Question3.java
@@ -1,4 +1,17 @@
 package question3;
 
+import java.util.Scanner;
 public class Question3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        System.out.println(n1 + " + " + n2 + " = " + (n1 + n2));
+        System.out.println(n1 + " - " + n2 + " = " + (n1 - n2));
+        System.out.println(n1 + " * " + n2 + " = " + (n1 * n2));
+        System.out.println(n1 + " / " + n2 + " = " + (n1 / n2));
+        System.out.println(n1 + " % " + n2 + " = " + (n1 % n2));
+        sc.close();
+
+    }
 }
---
+++ src/question4/Question4.java
@@ -0,0 +1,4 @@
+package question4;
+
+public class Question4 {
+}
