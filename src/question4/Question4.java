3e5364c57c546d44426352f47e070ceb85a9243b
yash.gautam_cs18 <yash.gautam_cs18@gla.ac.in>
2019-08-19 15:06:25 +0530

    add class:Qusetion5

--- src/question4/Question4.java
+++ src/question4/Question4.java
@@ -1,7 +1,18 @@
 package question4;
 
+import java.util.Scanner;
+
 public class Question4 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        if (n1 > 0)
+            System.out.println("Positive");
+        else if (n1 < 0)
+            System.out.println("Negative");
+        else if (n1 == 0)
+            System.out.println("Zero");
+        sc.close();
 
     }
 }
