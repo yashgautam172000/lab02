2062425c85580348a4167bbc0cb5dbd92226405e
yash.gautam_cs18 <yash.gautam_cs18@gla.ac.in>
2019-08-19 14:28:04 +0530

    add input for variable name:(name, String)

--- src/question1/Question1.java
+++ src/question1/Question1.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class Question1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine().trim();//example of method chaining
         sc.close();
 
     }
