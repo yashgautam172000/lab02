a4f3ffc9ce1936759e57096f3584b90093c557de
yash.gautam_cs18 <yash.gautam_cs18@gla.ac.in>
2019-08-19 14:45:14 +0530

    add outout statement

--- src/question2/Question2.java
+++ src/question2/Question2.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 public class Question2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String value1 = sc.next().trim();
+        String value2 = sc.next().trim();
+        System.out.println(value1 + " University " + value2);
+        sc.close();
 
     }
 }
